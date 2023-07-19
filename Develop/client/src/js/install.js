903c754709c24714acc96cdbe467f946bc35a6bc
Marq W <marquisewest86@duck.com>
2023-07-19 14:46:18 -0500

    added event listeners and handlers

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,46 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let deferredPrompt;
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default prompt
+  event.preventDefault();
+  
+  // Save the event for later use
+  deferredPrompt = event;
+  
+  // Display the install button or any other UI element to trigger the installation
+  butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  if (!deferredPrompt) {
+    return;
+  }
+
+  try {
+    // Show the installation prompt to the user
+    const result = await deferredPrompt.prompt();
+
+    // Wait for the user's choice
+    if (result.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  } catch (err) {
+    console.error('Error when prompting the user:', err);
+  }
+
+  // Reset the deferredPrompt to null after the user interaction
+  deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed');
+});
